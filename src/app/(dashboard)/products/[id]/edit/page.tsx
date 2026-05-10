e00c1dc452f6ea431c5b2a632bc448401f0874ca
MMohmedMahmoudd <mohmedmahmod257@gmail.com>
2026-05-11 02:42:21 +0300

    fix: switch to OpenNext adapter for Cloudflare Pages

--- src/app/(dashboard)/products/[id]/edit/page.tsx
+++ src/app/(dashboard)/products/[id]/edit/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-export const runtime = 'edge';
-
 import * as React from "react";
 import { useRouter, useParams } from "next/navigation";
 import { useForm } from "react-hook-form";
